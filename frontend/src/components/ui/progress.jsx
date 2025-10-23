d2c180a9d32129d43b559a0d3962aec9c90b4dba
MVPAlok <alokyadav5275757@gmail.com>
2025-10-23 21:55:23 +0530

    upgrade

--- frontend/src/components/ui/progress.jsx
+++ frontend/src/components/ui/progress.jsx
@@ -19,4 +19,4 @@ const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export { Progress }
-// End of progress.jsx
+// End of progress.jsx  
